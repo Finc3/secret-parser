25f2740dd66bf8250f228702814e0ceda8eb5488
Christopher Lege <christopher.lege@googlemail.com>
2020-10-22 18:18:38 +0200

    Updated the action so a custom value for the prefix and postfix can be supplied in case {{ is not a viable option

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import os, re, argparse
 
 
-def parser(filename, secret_name, secret_value):
+def parser(filename, secret_name, secret_value, format_prefix_postfix):
     """
     Parse the UTF-8 file and replace secret references with values from secrets dictionary.
     """
     print("Attempting to parse file: " + filename)
     with open('/github/workspace/' + filename, 'r') as fd:
         contents = fd.read()
-    references = re.findall("(\${{ *secrets.\w+ *}})", contents)
-    reference_names = [re.findall("\${{ *secrets.(\w+) *}}", x)[0] for x in references]
+    references = re.findall(f"(\${format_prefix_postfix} secrets.\w+ {format_prefix_postfix})", contents)
+    reference_names = [re.findall(f"\${format_prefix_postfix} secrets.(\w+) {format_prefix_postfix}", x)[0] for x in references]
     index = reference_names.index(secret_name)
     contents = secret_value.join(contents.split(references[index]))
     with open('/github/workspace/' + filename, 'w') as fd:
@@ -21,5 +21,6 @@ if __name__ == "__main__":
     argparser.add_argument('filename', help='file to parse')
     argparser.add_argument('secret_name', help='name of secret to search for')
     argparser.add_argument('secret_value', help='value of secret to replace name with')
+    argparser.add_argument('format_prefix_postfix', help="the format for the replacement string")
     args = argparser.parse_args()
-    parser(args.filename, args.secret_name, args.secret_value)
+    parser(args.filename, args.secret_name, args.secret_value, args.format_prefix_postfix)
